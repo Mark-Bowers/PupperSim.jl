b104f4ba784a7c2ee09144b9583b62ca6ed06dac
Mark Bowers <mark_bowers@live.com>
2020-10-13 21:02:48 -0700

    Joystick and keyboard handlers now use common set_ and adjust_ functions in QuadrupedController

--- src/joystick_handler.jl
+++ src/joystick_handler.jl
@@ -21,24 +21,13 @@ This function retrieves the state of the specified joystick remapped to an Xbox-
 If the specified joystick is not present or does not have a gamepad mapping this function will return GLFW_FALSE but will not generate an error. Call glfwJoystickPresent to check whether it is present regardless of whether it has a mapping.
 Not all devices have all the buttons or axes provided by GLFWgamepadstate. Unavailable buttons and axes will always report GLFW_RELEASE and 0.0 respectively.
 Possible errors include GLFW_NOT_INITIALIZED and GLFW_INVALID_ENUM.
-Added in version 3.3 (MWB !!! How can we make sure the user is using GLFW 3.3?)
+Added in version 3.3.
 =#
 
 GetGamepadState(joy::GLFW.Joystick, state) = Bool(ccall((:glfwGetGamepadState, GLFW.libglfw), Cint, (Cint, Ref{GLFWgamepadstate}), joy, state))
 
-# Functions ported over from JoystickInterface.py
-function deadband(value, band_radius)
-    return max(value - band_radius, 0) + min(value + band_radius, 0)
-end
-
-function clipped_first_order_filter(input, target, max_rate, tau)
-    rate = (target - input) / tau
-    return clamp(rate, -max_rate, max_rate)
-end
-
 function handle_scalar_settings(s::mjSim, buttons, axes)
     config  = s.robot.controller.config
-    state   = s.robot.state
     command = s.robot.command
 
     # Velocity
@@ -49,33 +38,15 @@ function handle_scalar_settings(s::mjSim, buttons, axes)
     command.yaw_rate = axes[AXIS_RIGHT_X] * -config.max_yaw_rate
 
     # Pitch
-    pitch = axes[AXIS_RIGHT_Y] * -config.max_pitch
-    #println("pitch: $pitch")
-    deadbanded_pitch = deadband(pitch, config.pitch_deadband)
-    pitch_rate = clipped_first_order_filter(
-        state.pitch,
-        deadbanded_pitch,
-        config.max_pitch_rate,
-        config.pitch_time_constant
-    )
-    #println("pitch_rate: $pitch_rate")
-    message_dt = 1.0 / s.refreshrate
-    command.pitch = state.pitch + message_dt * pitch_rate
-    #println("Setting command.pitch to $(command.pitch)")
+    set_pitch!(s.robot, axes[AXIS_RIGHT_Y])
 
     # Roll
     dpadx = Int(buttons[BUTTON_DPAD_RIGHT]) - Int(buttons[BUTTON_DPAD_LEFT])
-    if dpadx != 0
-        roll = state.roll + message_dt * config.roll_speed * dpadx
-        command.roll = clamp(roll, -1.0, 1.0)
-    end
+    dpadx != 0 && adjust_roll!(s.robot, dpadx)
 
     # Height
     dpady = Int(buttons[BUTTON_DPAD_UP]) - Int(buttons[BUTTON_DPAD_DOWN])
-    if dpady != 0
-        height = state.height - message_dt * config.z_speed * dpady
-        command.height = clamp(height, -0.25, -0.025)
-    end
+    dpady != 0 && adjust_height!(s.robot, dpady)
 end
 
 prev_buttons = @SVector fill(0x00, 15)
